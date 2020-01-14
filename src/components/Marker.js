5647a479542007bcb61b2f69c282c3d245ca8420
Jakub Ciepły <jakubc01@gmail.com>
2020-01-14 01:16:18 +0100

    Add colors for each marker depending on the air quality

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { measurements } from '../data/measurements.json';
 
-const Marker = ({ color, title, data }) => {
-  console.log(data);
+const Marker = ({ title, data }) => {
+  const { pm25 } = measurements.find(
+    (measurement) => data.id === measurement.id,
+  );
+
+  const chooseColor = () => {
+    if (pm25 < 10) return '#44A368';
+    if (pm25 < 20) return '#C1E080';
+    if (pm25 < 50) return '#C19330';
+    if (pm25 < 100) return '#E1625A';
+    if (pm25 >= 100) return '#7C1D7A';
+    return '#f11';
+  };
+
   return (
     <div>
       <div
         className="marker"
-        style={{ backgroundColor: color }}
+        style={{
+          backgroundColor: chooseColor(),
+          boxShadow: ` 0px 0px 28px 26px  ${chooseColor()}`,
+        }}
         title={title}
       />
       <div className="pulse" />
@@ -16,15 +32,10 @@ const Marker = ({ color, title, data }) => {
 };
 // NOTE: marker wywoluje sie przy component update
 // TODO: przy mouncie okreslic kolor
-Marker.defaultProps = {
-  color: '#000',
-};
 Marker.propTypes = {
   data: PropTypes.shape({
-    pm25: PropTypes.number,
-    pm10: PropTypes.number,
+    id: PropTypes.number.isRequired,
   }).isRequired,
-  color: PropTypes.string,
   title: PropTypes.string.isRequired,
 };
 export default Marker;
