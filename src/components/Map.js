904c91c95ddc7052ba2c5fec82b94df5cb433586
Jakub Ciepły <jakubc01@gmail.com>
2020-01-14 22:07:46 +0100

    Secure google map API key

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,13 +3,14 @@ import GoogleMap from 'google-map-react';
 import Marker from './Marker';
 import { sensors } from '../data/markers.json';
 import { measurements } from '../data/measurements.json';
+import key from '../data/key';
 
 const Map = () => (
   <div style={{ width: '50vw', height: '50vh' }}>
     <GoogleMap
       defaultCenter={{ lat: 50.264518, lng: 19.015257 }}
       defaultZoom={10}
-      bootstrapURLKeys={{ key: 'AIzaSyCh1H_l5l1fTYEbY8p8MvK8JZ9c5-1Y_3o' }}
+      bootstrapURLKeys={{ key }}
     >
       {sensors.map((sensor) => {
         const data = measurements.find(
