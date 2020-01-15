614278cf5885e2362ce1b6b6d1784b4d40231596
Jakub Ciepły <jakubc01@gmail.com>
2020-01-15 02:27:09 +0100

    Rebase map

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,17 +3,25 @@ import partner1 from '../img/MSK-logo.png';
 
 const Footer = () => (
   <footer>
-    <a href="https://facebook.com">
-      <i className="fab fa-facebook-square" />
-    </a>
-    <a href="https://instagram.com">
-      <i className="fab fa-instagram" />
-    </a>
-    <a href="/kontakt">Kontakt</a>
-    <a href="/aplikacja">Aplikacja</a>
-    <a href="/regulamin">Regulamin</a>
-    <h3>Nasi partnerzy: </h3>
-    <img src={partner1} alt="MSK" />
+    <div className="footer__icons">
+      <a href="https://facebook.com">
+        <i className="fab fa-facebook-square fa-4x" />
+      </a>
+      <a href="https://instagram.com">
+        <i className="fab fa-instagram fa-4x" />
+      </a>
+    </div>
+    <div className="footer__links">
+      <a href="/kontakt">Kontakt</a>
+      <a href="/aplikacja">Aplikacja</a>
+      <a href="/regulamin">Regulamin</a>
+    </div>
+    <h3 className="footer__center">Nasi partnerzy: </h3>
+    <div className="footer__partner">
+      <img src={partner1} alt="MSK" />
+      <img src={partner1} alt="MSK" />
+      <img src={partner1} alt="MSK" />
+    </div>
   </footer>
 );
 
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import InfoCard from './InfoCard';
-import { content } from '../data/landing-page.json';
+import React from "react";
+import InfoCard from "./InfoCard";
+import { content } from "../data/landing-page.json";
 
 const LandingPage = () => (
-  <>
+  <main className="landing-page">
     <img src="https://picsum.photos/280/210" alt="Logo" />
-    <h1>Feel the air</h1>
-    <h2>{content.h2}</h2>
-    <a href="/kampania">Zobacz jak</a>
+    <div className="air">
+      <h1>Feel the air</h1>
+      <h2>{content.h2}</h2>
+      <a className="air__btn" href="/kampania">
+        Zobacz jak
+      </a>
+    </div>
     <InfoCard
       className="project"
       header="Nasz projekt"
@@ -15,14 +19,16 @@ const LandingPage = () => (
     />
     <div className="map">
       <div className="map__header">
-        <h2>Mapa z pomiarami z naszych czujników</h2>
+        <h2 className="map__h">Mapa z pomiarami z naszych czujników</h2>
+      </div>
+      <div className="map__img">
         <img src="https://picsum.photos/100/130" alt="Mapa" />
       </div>
       <div className="map__content">
-        <p>{content.p}</p>
+        <p className="map__p">{content.p}</p>
       </div>
     </div>
-  </>
+  </main>
 );
 
 export default LandingPage;
