79503bc850cdd85e2b992dc973d9bf285a095c13
kubusjestsuper <58520735+kubusjestsuper@users.noreply.github.com>
2020-01-15 02:17:05 +0100

    Style landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,14 +2,16 @@ import React from 'react';
 import InfoCard from './InfoCard';
 
 const LandingPage = () => (
-  <>
+  <main className="landing-page">
     <img src="https://picsum.photos/280/210" alt="Logo" />
-    <h1>Feel the air</h1>
-    <h2>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit accusamus
-      illo beatae!
-    </h2>
-    <a href="/kampania">Zobacz jak</a>
+    <div className="air">
+      <h1>Feel the air</h1>
+      <h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit accusamus
+        illo beatae!
+      </h2>
+      <a className="air__btn" href="/kampania">Zobacz jak</a>
+    </div>
     <InfoCard
       className="project"
       header="Nasz projekt"
@@ -20,17 +22,19 @@ const LandingPage = () => (
     />
     <div className="map">
       <div className="map__header">
-        <h2>Mapa z pomiarami z naszych czujników</h2>
+        <h2 className="map__h">Mapa z pomiarami z naszych czujników</h2>
+      </div>
+      <div className="map__img">
         <img src="https://picsum.photos/100/130" alt="Mapa" />
       </div>
       <div className="map__content">
-        <p>
+        <p className="map__p">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum!
           Lorem ipsum dolor sit amet.
         </p>
       </div>
     </div>
-  </>
+  </main>
 );
 
 export default LandingPage;
