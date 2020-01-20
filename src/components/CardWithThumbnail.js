6ffd47adaae97f01a844d8818153cce2837c56ff
Jakub Ciepły <jakubc01@gmail.com>
2020-01-21 00:25:35 +0100

    Create partners page and refactor CardWithThumbnail to HOC

---
+++ src/components/CardWithThumbnail.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// prettier-ignore
+const CardWithThumbnail = ({
+  thumbnail, header, description, env,
+}) => (
+  <>
+    <img className={`${env}__thumbnail`} src={thumbnail} alt="Miniaturka" />
+    <h3 className={`${env}__header`}>{header}</h3>
+    <p className={`${env}__description`}>{description}</p>
+  </>
+);
+CardWithThumbnail.propTypes = {
+  thumbnail: PropTypes.string.isRequired,
+  header: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  env: PropTypes.string.isRequired,
+};
+export default CardWithThumbnail;
--- src/components/Partners.js
+++ src/components/Partners.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { partners } from '../data/partners.json';
+import CardWithThumbnail from './CardWithThumbnail';
 
 const Partners = () => (
-  <div>
+  <div className="partners">
     <h1>Nasi Partnerzy</h1>
+    {partners.map((partner) => (
+      <CardWithThumbnail
+        key={partner.name}
+        header={partner.name}
+        description={partner.description}
+        thumbnail={partner.thumbnail}
+        env="partners"
+      />
+    ))}
   </div>
 );
 
