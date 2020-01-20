05b48b1e165a404bc358bded757886612a0a9776
Jakub Ciepły <jakubc01@gmail.com>
2020-01-20 21:16:03 +0100

    Create Contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
-const Contact = () => <div />;
+const Contact = () => (
+  <div>
+    <h1>Skontaktuj się z nami</h1>
+    <h3>
+      Masz pomysł jak usprawnić działanie naszej strony? Chchaiłbyś zakupić nasz
+      czujnik? Jeśli tak wypełnij formularz kontaktowy
+    </h3>
+    <form>
+      <input type="text" placeholder="Adres e-mail" />
+      <input type="text" placeholder="Temat" />
+      <input type="textarea" placeholder="Treść" />
+      <button type="submit">Wyślij</button>
+    </form>
+  </div>
+);
 
 export default Contact;
