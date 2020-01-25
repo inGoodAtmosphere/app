027ce2d32a0ebe52dc858506b9a468ea7bb0c3f4
Jakub Ciepły <jakubc01@gmail.com>
2020-01-26 00:02:06 +0100

    Fix git bug

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,39 +1,82 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import Slider from 'react-slick';
+import Carousel from 'nuka-carousel';
 import AboutUsCard from '../components/AboutUsCard';
 import { content } from '../data/about-us.json';
+import useWindowWidth from '../hooks/useWindowWidth';
 
 const AboutUs = () => {
-  const settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    // lazyLoad: true,
-    responsive: [
+  const width = useWindowWidth();
+  const setBreakpoints = () => {
+    const breakpoints = [
       {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 3,
-          arrows: false,
+        slidesToShow: 1,
+      },
+      {
+        slidesToShow: 2,
+      },
+      {
+        slidesToShow: 3,
+      },
+      {
+        slidesToShow: 4,
+        renderCenterLeftControls({ previousSlide }) {
+          return (
+            <button
+              type="button"
+              className="slider__btn"
+              onClick={previousSlide}
+            >
+              <i className="fas fa-arrow-left" />
+            </button>
+          );
+        },
+        renderCenterRightControls({ nextSlide }) {
+          return (
+            <button type="button" className="slider__btn" onClick={nextSlide}>
+              <i className="fas fa-arrow-right" />
+            </button>
+          );
         },
       },
-    ],
+    ];
+    if (width < 400) return breakpoints[0];
+    if (width < 600) return breakpoints[1];
+    if (width < 1000) return breakpoints[2];
+    return breakpoints[3];
   };
-
   return (
-    <>
-      <Slider {...settings}>
+    <div className="wrapper">
+      <h1>O nas</h1>
+      <Carousel
+        enableKeyboardControls
+        cellSpacing={-40}
+        slidesToShow={1}
+        wrapAround
+        transitionMode="scroll3d"
+        renderCenterLeftControls={() => null}
+        renderCenterRightControls={() => null}
+        renderBottomCenterControls={() => null}
+        {...setBreakpoints()}
+      >
         <AboutUsCard image="https://picsum.photos/200/275" content={content} />
+
         <AboutUsCard image="https://picsum.photos/200/275" content={content} />
+
         <AboutUsCard image="https://picsum.photos/200/275" content={content} />
+
         <AboutUsCard image="https://picsum.photos/200/275" content={content} />
+
         <AboutUsCard image="https://picsum.photos/200/275" content={content} />
+
         <AboutUsCard image="https://picsum.photos/200/275" content={content} />
-      </Slider>
-    </>
+
+        <AboutUsCard image="https://picsum.photos/200/275" content={content} />
+
+        <AboutUsCard image="https://picsum.photos/200/275" content={content} />
+      </Carousel>
+    </div>
   );
 };
-
 export default AboutUs;
