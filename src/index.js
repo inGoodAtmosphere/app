402658641f65d30a4a727e53c2cd348dae7fe158
Jakub Ciepły <jakubc01@gmail.com>
2020-01-26 13:36:24 +0100

    Remove unnecessary packages

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import 'normalize.css';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+
 import './styles/index.scss';
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
