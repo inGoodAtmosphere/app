31517aa5f8a3a91e8218b292d3f6c561b62cd933
Jakub Ciepły <jakubc01@gmail.com>
2020-01-27 18:33:51 +0100

    Change about-us card to grid

--- src/components/AboutUsCard.js
+++ src/components/AboutUsCard.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const AboutUsCard = ({ image, content: { name, role, description } }) => (
-  <div className="card about-us__card">
+  <div className="about-us__card__wrapper">
     <img className="about-us__card__image" src={image} alt={name} />
-
+    <div className="card about-us__card" />
     <div className="about-us__card__description">
       <h2>{name}</h2>
       <h3>{role}</h3>
