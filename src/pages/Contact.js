bc20f8267fcabc0103cbcaa28c59b1b5e8fd9d69
Jakub Ciepły <jakubc01@gmail.com>
2020-01-30 14:40:49 +0100

    Create contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,18 +1,38 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faInstagram,
+  faFacebookSquare,
+} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Contact = () => (
   <main className="content">
     <h1>Skontaktuj się z nami</h1>
-    <h3>
-      Masz pomysł jak usprawnić działanie naszej strony? Chchaiłbyś zakupić nasz
-      czujnik? Jeśli tak wypełnij formularz kontaktowy
+    <h3 className="contact__h3">
+      Masz pomysł jak usprawnić działanie naszej strony? Chchaiłbyś współtworzyć
+      naszego bloga? Jeśli tak napisz tutaj:
     </h3>
-    <form>
-      <input type="text" placeholder="Adres e-mail" />
-      <input type="text" placeholder="Temat" />
-      <input type="textarea" placeholder="Treść" />
-      <button type="submit">Wyślij</button>
-    </form>
+    <ul className="contact__list">
+      <li className="contact__list__item">
+        <a href="mailto:ingoodatmosphere@gmail.com">
+          <FontAwesomeIcon icon={faEnvelope} size="2x" />
+          <p>ingoodatmosphere@gmail.com</p>
+        </a>
+      </li>
+      <li className="contact__list__item">
+        <a href="https://facebook.com/ingoodatmosphere@gmail">
+          <FontAwesomeIcon icon={faFacebookSquare} size="2x" />
+          <p>inGoodAtmosphere</p>
+        </a>
+      </li>
+      <li className="contact__list__item">
+        <a href="https://instagram.com/in.good.atmosphere?igshid=1ehmhou6voq0s">
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+          <p>inGoodAtmosphere</p>
+        </a>
+      </li>
+    </ul>
   </main>
 );
 
