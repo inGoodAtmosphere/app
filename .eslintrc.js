f7c7775159b5ffb1797d8fe9874a65af8fc18fb5
Jakub Ciepły <jakubc01@gmail.com>
2020-02-01 22:08:47 +0100

    PWA is working

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,5 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['react'],
-  rules: {},
+  "ignorePatterns": ["serviceWorker.js", "build/"],
 };
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
 import AppRouter from './routers/AppRouter';
 import 'normalize.css';
+import * as serviceWorker from './serviceWorker';
 import './styles/index.scss';
 
 ReactGA.initialize('UA-156974442-1');
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
+serviceWorker.register();
