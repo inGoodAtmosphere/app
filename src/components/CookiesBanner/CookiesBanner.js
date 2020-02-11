fb1492120582499679c8ff13091c892534400d0a
Jakub Ciepły <jakubc01@gmail.com>
2020-02-11 22:54:16 +0100

    Implement cookies for cookies banner

---
+++ src/components/CookiesBanner/CookiesBanner.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useCookies } from 'react-cookie';
+import './cookies-banner.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+const CookiesBanner = () => {
+  const [cookies, setCookie] = useCookies(['isOpenBanner']);
+  return (
+    <div
+      className={
+        cookies.isOpenBanner === 'false'
+          ? 'cookies-banner__wrapper--hidden'
+          : 'cookies-banner__wrapper'
+      }
+    >
+      <button
+        type="button"
+        className="cookies-banner__btn"
+        // prettier-ignore
+        onClick={() => setCookie('isOpenBanner', 'false', {
+          path: '/',
+          expires: new Date(2021, 1, 1),
+        })}
+      >
+        <FontAwesomeIcon icon={faTimes} />
+      </button>
+      <p className="cookies-banner__text">
+        W celu świadczenia usług na najwyższym poziomie stosujemy pliki cookies.
+        W każdym momencie możesz dokonać zmiany ustawień przeglądarki.
+      </p>
+
+      <a
+        href="/polityka-prywatnosci"
+        aria-label="Polityka prywatności"
+        className="cookies-banner__link"
+      >
+        Zobacz politykę prywatności
+      </a>
+    </div>
+  );
+};
+
+export default CookiesBanner;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
 import { Router, Route, Switch } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
 import history from '../history/history';
 
 import 'normalize.css';
@@ -18,7 +19,7 @@ import Sensors from '../pages/Sensors/Sensors';
 import Contact from '../pages/Contact/Contact';
 import Encyclopedia from '../pages/Encyclopedia/Encyclopedia';
 import NotFoundPage from '../pages/NotFound/NotFound';
-import CookiesWarning from '../components/CookiesWarning/CookiesWarning';
+import CookiesBanner from '../components/CookiesBanner/CookiesBanner';
 
 const Routes = () => {
   history.listen((location) => {
@@ -31,22 +32,24 @@ const Routes = () => {
   }, []);
   return (
     <Router history={history}>
-      <div className="container">
-        <Header />
-        <Switch>
-          <Route path="/" component={LandingPage} exact />
-          <Route path="/o-nas" component={AboutUs} />
-          <Route path="/kampania" component={Campaign} exact />
-          <Route path="/kampania/:header" component={Article} />
-          <Route path="/partnerzy" component={Partners} />
-          <Route path="/czujniki" component={Sensors} />
-          <Route path="/kontakt" component={Contact} />
-          <Route path="/encyklopedia" component={Encyclopedia} />
-          <Route component={NotFoundPage} />
-        </Switch>
-        <CookiesWarning />
-        <Footer />
-      </div>
+      <CookiesProvider>
+        <div className="container">
+          <Header />
+          <Switch>
+            <Route path="/" component={LandingPage} exact />
+            <Route path="/o-nas" component={AboutUs} />
+            <Route path="/kampania" component={Campaign} exact />
+            <Route path="/kampania/:header" component={Article} />
+            <Route path="/partnerzy" component={Partners} />
+            <Route path="/czujniki" component={Sensors} />
+            <Route path="/kontakt" component={Contact} />
+            <Route path="/encyklopedia" component={Encyclopedia} />
+            <Route component={NotFoundPage} />
+          </Switch>
+          <CookiesBanner />
+          <Footer />
+        </div>
+      </CookiesProvider>
     </Router>
   );
 };
