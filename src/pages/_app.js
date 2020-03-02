629ff70af7a044cb321f3f0d44a2f6e699079b0b
Jakub Ciepły <jakubc01@gmail.com>
2020-03-02 14:57:11 +0100

    Add properly HTML

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react';
+import { config } from '@fortawesome/fontawesome-svg-core';
 import PropTypes from 'prop-types';
 import cookies from 'next-cookies';
+import Head from 'next/head';
 import CookiesBanner from '../components/CookiesBanner/CookiesBanner';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import { initGA, logPageView } from '../utils/analytics';
 import '../styles/index.scss';
 import 'normalize.css';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import 'focus-visible';
+
+config.autoAddCss = false;
 
 const App = ({ Component, isBannerOpen }) => {
   useEffect(() => {
@@ -20,6 +25,52 @@ const App = ({ Component, isBannerOpen }) => {
   initGA();
   return (
     <>
+      <Head>
+        <meta key="charset" charSet="utf-8" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap&subset=latin-ext"
+          rel="preload"
+          as="font"
+          type="font/woff2"
+        />
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <meta
+          key="description"
+          name="description"
+          content="inGoodAtmosphere ma na celu stworzenie przyjaznej atmosfery wolnej od smogu w całej Polsce!"
+        />
+        <link rel="icon" href="/icons/favicon-32.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="icons/favicon-180.png" />
+        <meta key="theme-color" name="theme-color" content="#1C233C" />
+        <meta
+          key="apple-mobile-web-app-status-bar"
+          name="apple-mobile-web-app-status-bar"
+          content="#1c233c"
+        />
+        <meta
+          key="og:url"
+          property="og:url"
+          content="https://www.ingoodatmosphere.com/"
+        />
+        <meta key="og:type" property="og:type" content="website" />
+        <meta key="og:title" property="og:title" content="inGoodAtmosphere" />
+        <meta
+          key="og:description"
+          property="og:description"
+          content='Realizujemy projekt w ramach olimpiady "Zwolnieni z Teorii". Celem naszego projektu, jest uświadomienie ludzi o szkodliwości smogu i jego wpływu na nasze życie, organizm oraz niebezpieczeństwo dla środowiska.'
+        />
+        <meta
+          key="og:image"
+          property="og:image"
+          content="https://ingoodatmosphere.com/img/opengraph-homepage.jpg"
+        />
+        <title>inGoodAtmosphere</title>
+      </Head>
       <div className="container">
         <Header />
         <Component />
--- src/pages/kampania/Naturalne-wskazniki-zanieczyszczenia-powietrza.js
+++ src/pages/kampania/Naturalne-wskazniki-zanieczyszczenia-powietrza.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 import './article.module.scss';
 import { logEvent } from '../../utils/analytics';
 
@@ -21,34 +22,74 @@ const Article = () => {
   }, []);
   return (
     article && (
-      <main className="content">
-        <article className="card article__card">
-          <div className="article__header">
-            <h1 className="article__header__text">{article.header}</h1>
-            <img
-              className="article__header__img"
-              src={article.image[1]}
-              alt="Miniaturka artykułu"
-              srcSet={`${article.image[0]} 768w, ${article.image[1]} 1280w, ${article.image[2]} 1920w, ${article.image[3]} 5319w`}
-            />
-          </div>
-          {article.subHeaders
-            ? article.subHeaders.map((subHeader, i) => (
-              <section key={subHeader}>
-                <h2>{subHeader}</h2>
-                <p>{article.contents[i]}</p>
-              </section>
-            ))
-            : article.contents[0].split('\n').map((paragraph) => (
-              <p
-                className="article__paragraph"
-                key={paragraph.substring(1, 12)}
-              >
-                {paragraph}
-              </p>
-            ))}
-        </article>
-      </main>
+      <>
+        <Head>
+          <meta
+            key="og:url"
+            property="og:url"
+            content="https://www.ingoodatmosphere.com/kampania/Naturalne-wskazniki-zanieczyszczenia-powietrza"
+          />
+          <meta key="og:type" property="og:type" content="article" />
+          <meta
+            key="article:published_time"
+            property="article:published_time"
+            content="2020-02-19"
+          />
+          <meta
+            key="article:author"
+            property="article:author"
+            content="Julia Papée"
+          />
+          <meta
+            key="article:section"
+            property="article:section"
+            content="Ekologia"
+          />
+          <meta
+            key="og:title"
+            property="og:title"
+            content="Naturalne wskaźniki zanieczyszczenia powietrza"
+          />
+          <meta
+            key="og:description"
+            property="og:description"
+            content="Aby mierzyć zanieczyszczenie powietrza wcale nie potrzebujemy sprzętu za duże pieniądze. Dowiedz się czym są bioindykatory i jak można dzięki nim określić jakość powietrza w okolicy"
+          />
+          <meta
+            key="og:image"
+            property="og:image"
+            content="https://ingoodatmosphere.com/data/articles/bioindicators/bioindicators-1280.jpg"
+          />
+        </Head>
+        <main className="content">
+          <article className="card article__card">
+            <div className="article__header">
+              <h1 className="article__header__text">{article.header}</h1>
+              <img
+                className="article__header__img"
+                src={article.image[1]}
+                alt="Miniaturka artykułu"
+                srcSet={`${article.image[0]} 768w, ${article.image[1]} 1280w, ${article.image[2]} 1920w, ${article.image[3]} 5319w`}
+              />
+            </div>
+            {article.subHeaders.length
+              ? article.subHeaders.map((subHeader, i) => (
+                <section key={subHeader}>
+                  <h2>{subHeader}</h2>
+                  <p>{article.contents[i]}</p>
+                </section>
+                ))
+              : article.contents[0].split('\n').map((paragraph) => (
+                <p
+                  className="article__paragraph"
+                  key={paragraph.substring(1, 12)}
+                >
+                  {paragraph}
+                </p>
+                ))}
+          </article>
+        </main>
+      </>
     )
   );
 };
