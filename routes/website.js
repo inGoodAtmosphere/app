c92c704b2ad44e0f7a63be72e562cc9db8acd62e
obx88 <33556172+obx88@users.noreply.github.com>
2020-03-03 23:38:12 +0100

    code cleanup

---
+++ routes/website.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const next = require('next');
+const app = next({ dev });
+const router = express.router
+
+router.get('/', (req, res) => {
+  return app.render(req, res, '/', req.query);
+});
+
+router.get('/kampania', (req, res) => {
+  return app.render(req, res, '/kampania', req.query);
+});
+router.get('/o-nas', (req, res) => {
+  return app.render(req, res, '/o-nas', req.query);
+});
+router.get('/encyklopedia', (req, res) => {
+  return app.render(req, res, '/encyklopedia', req.query);
+});
+router.get('/mierniki', (req, res) => {
+  return app.render(req, res, '/mierniki', req.query);
+});
+router.get('/kontakt', (req, res) => {
+  return app.render(req, res, '/kontakt', req.query);
+});
+router.get('/polityka-prywatnosci', (req, res) => {
+  return app.render(req, res, '/polityka-prywatnosci', req.query);
+});
