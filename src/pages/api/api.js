a1ca159d925ba8625a06e1b5560ca030fe212a27
obx88 <33556172+obx88@users.noreply.github.com>
2020-03-12 15:15:06 +0100

    testing env developed

--- src/pages/api/api.js
+++ src/pages/api/api.js
@@ -3,8 +3,13 @@
 // const {router} = express;
 
 const requests = [];
+let emptyRequests = 0;
 
 export default (req, res) => {
-  requests.push(req.body);
-  res.send(requests);
+  if(req.body ==="clear"){
+    requests.slice(0,requests.length);
+    emptyRequests=0;
+  }
+  else if(req.body==="") requests.push(req.body);
+  else res.send(requests);
 };
