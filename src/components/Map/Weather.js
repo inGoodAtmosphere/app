b39d118ac7f5fb98e264931a76dc9417f1d8ffea
Jakub Ciepły <jakubc01@gmail.com>
2020-03-12 21:00:50 +0100

    Add Weather component

--- src/components/Map/Weather.js
+++ src/components/Map/Weather.js
@@ -1,7 +1,27 @@
-import React from 'react';
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { useContext } from 'react';
+import mapContext from '../../utils/map-context';
+import './weather.module.scss';
 
 const Weather = () => {
-  return <div>Weather</div>;
+  const {
+    activeSensor: { temperature, humidity },
+  } = useContext(mapContext);
+  return (
+    <div className="card map__weather">
+      <div className="map__weather__temperature map__weather__data">
+        <span>Temperatura</span>
+        <span>
+          {temperature}
+          &#8451;
+        </span>
+      </div>
+      <div className="map__weather__humidity map__weather__data">
+        <span>Wilgotność</span>
+        <span>{humidity}%</span>
+      </div>
+    </div>
+  );
 };
 
 export default Weather;
