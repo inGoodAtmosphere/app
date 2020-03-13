51b8435d6b17715508c76a45c103ee92dfb6dccc
obx88 <33556172+obx88@users.noreply.github.com>
2020-03-13 13:23:32 +0100

    bug fixed

--- src/pages/api/api.js
+++ src/pages/api/api.js
@@ -6,11 +6,10 @@ const requests = [];
 let emptyRequests = 0;
 
 export default (req, res) => {
-  if(req.body ==="clear"){
-    requests.slice(0,requests.length);
-    emptyRequests=0;
-  }
-  else if(req.body==="") emptyRequests+=1;
-  else requests.push(req.body)
+  if (req.body === 'clear') {
+    requests.splice(0, requests.length);
+    emptyRequests = 0;
+  } else if (req.body === '') emptyRequests += 1;
+  else requests.push(req.body);
   res.send(`${emptyRequests}\r\n${requests}`);
 };
