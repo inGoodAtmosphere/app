125632ca8d1464953a6d9dc4f7a891e6be9e5436
Jakub Ciepły <jakubc01@gmail.com>
2020-03-13 14:31:14 +0100

    Create support page

--- src/components/LandingPage/Cards.js
+++ src/components/LandingPage/Cards.js
@@ -5,10 +5,18 @@ import MapSvg from './svg/Map';
 import EncyclopediaSvg from './svg/Encyclopedia';
 import SensorSvg from './svg/Sensor';
 import ContactSvg from './svg/Contact';
+import SupportSvg from './svg/Support';
 import { content } from '../../../public/data/landing-page.json';
 
 const Cards = () => {
-  const SVGs = [ProjectSvg, MapSvg, EncyclopediaSvg, SensorSvg, ContactSvg];
+  const SVGs = [
+    ProjectSvg,
+    MapSvg,
+    EncyclopediaSvg,
+    SensorSvg,
+    ContactSvg,
+    SupportSvg,
+  ];
   return content.map((card, i) => (
     <InfoCard
       key={card.purpose}
---
+++ src/pages/wsparcie/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Support = () => {
+  return (
+    <main className="content">
+      <h1>Wesprzyj nas</h1>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing.</h2>
+      <div>donorbox</div>
+    </main>
+  );
+};
+
+export default Support;
