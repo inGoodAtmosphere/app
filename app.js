6cbf967ee2407c2d71698ed40d02d680d52773db
obx88 <33556172+obx88@users.noreply.github.com>
2020-03-13 19:01:14 +0100

    router deleted

--- app.js
+++ app.js
@@ -5,14 +5,31 @@ const port = process.env.PORT || 3000;
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handle = app.getRequestHandler();
 
-// ROUTERS
-const websiteRouter = require('./routes/website.js'); // Router of next.js
-
 app.prepare().then(() => {
   const server = express();
 
-  // Routers as middleware
-  server.use('/routes', websiteRouter);
+  server.get('/', (req, res) => {
+    return app.render(req, res, '/', req.query);
+  });
+
+  server.get('/kampania', (req, res) => {
+    return app.render(req, res, '/kampania', req.query);
+  });
+  server.get('/o-nas', (req, res) => {
+    return app.render(req, res, '/o-nas', req.query);
+  });
+  server.get('/encyklopedia', (req, res) => {
+    return app.render(req, res, '/encyklopedia', req.query);
+  });
+  server.get('/mierniki', (req, res) => {
+    return app.render(req, res, '/mierniki', req.query);
+  });
+  server.get('/kontakt', (req, res) => {
+    return app.render(req, res, '/kontakt', req.query);
+  });
+  server.get('/polityka-prywatnosci', (req, res) => {
+    return app.render(req, res, '/polityka-prywatnosci', req.query);
+  });
 
   server.all('*', (req, res) => {
     return handle(req, res);
