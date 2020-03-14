87683315350acf66076e7942b02889db93e815aa
Jakub Ciepły <jakubc01@gmail.com>
2020-03-14 12:57:20 +0100

    Add fetching data to map

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,24 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import GoogleMap from 'google-map-react';
 import Marker from './Marker';
-import { sensors } from '../../../public/data/markers.json';
-import { measurements } from '../../../public/data/measurements.json';
 import key from '../../../key';
 import './map.module.scss';
 
-const Map = () => (
-  <div className="map">
-    <GoogleMap
-      defaultCenter={{ lat: sensors[0].lat, lng: sensors[0].lng }}
-      defaultZoom={15}
-      bootstrapURLKeys={{ key }}
-    >
-      {sensors.map(({ id, lat, lng }) => {
-        const data = measurements.find((measurement) => measurement.id === id);
-        return <Marker value={{ data }} key={id} lat={lat} lng={lng} />;
-      })}
-    </GoogleMap>
-  </div>
-);
+const Map = () => {
+  const [sensors, setSensors] = useState([]);
+  const [measurements, setMeasurements] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(true);
+  const [isLoadedMeasurements, setIsLoadedMeasurements] = useState(true);
+  const [isError, setIsError] = useState(false);
+  useEffect(() => {
+    fetch('/api/markers')
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setSensors(result);
+          setIsLoaded(false);
+        },
+        () => {
+          setIsError(true);
+        },
+      )
+      .then(() =>
+        fetch('/api/measurements').then((res) =>
+          res.json().then(
+            (result) => {
+              setMeasurements(result);
+              setIsLoadedMeasurements(false);
+            },
+            () => {
+              setIsError(true);
+            },
+          ),
+        ),
+      );
+  }, []);
+  if (isLoaded) return <p>Ładowanie</p>;
+  if (isError) return <p>Błąd</p>;
+
+  return (
+    <div className="map">
+      <GoogleMap
+        defaultCenter={{ lat: sensors[0].lat, lng: sensors[0].lng }}
+        defaultZoom={15}
+        bootstrapURLKeys={{ key }}
+      >
+        {!isLoadedMeasurements &&
+          sensors.map(({ id, lat, lng }) => {
+            const data = measurements.find(
+              (measurement) => measurement.id === id,
+            );
+            return <Marker value={{ data }} key={id} lat={lat} lng={lng} />;
+          })}
+      </GoogleMap>
+    </div>
+  );
+};
 
 export default Map;
