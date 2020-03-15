f757860c8b29c4b51cf17d7c33d80ea815586d3a
Jakub Ciepły <jakubc01@gmail.com>
2020-03-15 22:54:03 +0100

    Delete unnecessary files from api

--- src/pages/api/articles/bioindicators.js
+++
@@ -1,7 +0,0 @@
-import { article } from '../../../../public/data/articles/Naturalne-wskazniki-zanieczyszczenia-powietrza.json';
-
-export default (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(article));
-};
