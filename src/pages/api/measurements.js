9cf05a1fb89c3f407ca803aa8d8ebf7e9f4cb40b
obx88 <33556172+obx88@users.noreply.github.com>
2020-03-16 12:34:01 +0100

    db typos fixed

--- src/pages/api/measurements.js
+++ src/pages/api/measurements.js
@@ -18,7 +18,7 @@ db.connect((err) => {
 export default (req, res) => {
   // 'Select m.id, m.device_id, m.measurment_datetime, m.humidity, m.temperature, m.pm1, m.pm2_5, m.pm10 from measurments as m INNER JOIN (SELECT max(id) as id, `device_id` FROM `measurments` GROUP BY device_id ORDER BY `measurments`.`device_id` ASC) as maxId on maxId.id=m.id ORDER BY `device_id` ASC;'
   const queryContent =
-    'Select m.id, m.device_id, m.measurement_datetime, m.humidity, m.temperature, m.pm1, m.pm25, m.pm10 from measurements as m INNER JOIN (SELECT max(id) as id, `device_id` FROM `measurments` GROUP BY device_id ORDER BY `measurements`.`device_id` ASC) as maxId on maxId.id=m.id ORDER BY `device_id` ASC;';
+    'Select m.id, m.device_id, m.measurement_datetime, m.humidity, m.temperature, m.pm1, m.pm25, m.pm10 from measurements as m INNER JOIN (SELECT max(id) as id, `device_id` FROM `measurements` GROUP BY device_id ORDER BY `measurements`.`device_id` ASC) as maxId on maxId.id=m.id ORDER BY `device_id` ASC;';
   db.query(queryContent, (err, result) => {
     if (err) {
       res.json({
