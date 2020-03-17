cdafc33dfc87c052eca5954a7bb2c33b91c57eb6
Jakub Ciepły <jakubc01@gmail.com>
2020-03-17 13:51:09 +0100

    Add Botland to partners

--- src/components/Footer/Patrons.js
+++ src/components/Footer/Patrons.js
@@ -22,12 +22,22 @@ const Patrons = () => {
             img="/img/tvp.png"
           />
         </div>
+
         <div className="footer__patron">
           <h3>Patroni: </h3>
           <Patron
             name="Młodzieżowy Strajk Klimatyczny"
             link="https://www.msk.earth/"
             img="/img/msk.png"
+          />
+        </div>
+        <div className="break" />
+        <div className="footer__patron">
+          <h3>Partnerzy: </h3>
+          <Patron
+            name="Botland"
+            link="https://botland.com.pl/pl/"
+            img="/img/botland.png"
           />
         </div>
       </div>
