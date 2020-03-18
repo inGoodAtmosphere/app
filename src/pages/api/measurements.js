c737d43bd214f577aae6ca27b3d3fedb5596f1c8
obx88 <33556172+obx88@users.noreply.github.com>
2020-03-18 23:39:02 +0100

    added post method handling

--- src/pages/api/measurements.js
+++ src/pages/api/measurements.js
@@ -1,6 +1,10 @@
 const dbQuery = require('../../../api_modules/selectQuery');
 
 export default async (req, res) => {
+  if (req.method !== 'GET') {
+    res.status(403);
+    res.end();
+  }
   const query =
     "Select m.id, m.device_id, m.measurement_date as measurementDate, m.measurement_time as measurementTime, m.humidity, m.temperature, m.pm1, m.pm25 as 'pm2.5', m.pm10 from measurements as m INNER JOIN (SELECT max(id) as id, `device_id` FROM `measurements` GROUP BY device_id ORDER BY `measurements`.`device_id` ASC) as maxId on maxId.id=m.id ORDER BY `device_id` ASC;";
   const result = await dbQuery(query);
--- src/pages/api/measurements/[id].js
+++ src/pages/api/measurements/[id].js
@@ -1,10 +1,23 @@
 const dbQuery = require('../../../../api_modules/selectQuery');
 
 export default async (req, res) => {
+  let query = '';
+  let result = '';
   const {
     query: { id },
   } = req;
-  const query = `SELECT ID as id, measurement_date as measurementDate, round(avg(humidity)) as humidity ,round(avg(temperature)) as temperature ,round(avg(pm1)) as pm1, round(avg(pm25)) as 'pm2.5' ,round(avg(pm10)) as pm10  FROM measurements WHERE device_id=${id} GROUP BY measurement_date;`;
-  const result = await dbQuery(query);
+  switch (req.method) {
+    case 'GET':
+      query = `SELECT ID as id, measurement_date as measurementDate, round(avg(humidity)) as humidity ,round(avg(temperature)) as temperature ,round(avg(pm1)) as pm1, round(avg(pm25)) as 'pm2.5' ,round(avg(pm10)) as pm10  FROM measurements WHERE device_id=${id} GROUP BY measurement_date;`;
+      result = await dbQuery(query);
+      break;
+    case 'POST':
+      query = `INSERT INTO measurements(device_id,measurement_date, measurement_time, receive_date,receive_time, humidity, temperature, pm1,pm25,pm10) values(${id},${req.body.measurementDate},${req.body.measurementTime},CURRENT_DATE, CURRENT_TIME, ${req.body.humidity},${req.body.temperature},${req.body.pm1},${req.body.pm25},${req.body.pm10})`;
+      result = await dbQuery(query);
+      break;
+    default:
+      res.status(403);
+      res.end();
+  }
   res.send(result);
 };
