2e0695fa159dd993cbaaaee4a7945ee7dd989456
obx88 <33556172+obx88@users.noreply.github.com>
2020-03-18 19:18:35 +0100

    rounded avg results

--- src/pages/api/measurements/[id].js
+++ src/pages/api/measurements/[id].js
@@ -4,7 +4,7 @@ export default async (req, res) => {
   const {
     query: { id },
   } = req;
-  const query = `SELECT ID as id, measurement_date as measurementDate, avg(humidity) as humidity ,avg(temperature) as temperature ,avg(pm1) as pm1, avg(pm25) as 'pm2.5' ,avg(pm10) as pm10  FROM measurements WHERE device_id=${id} GROUP BY measurement_date;`;
+  const query = `SELECT ID as id, measurement_date as measurementDate, round(avg(humidity)) as humidity ,round(avg(temperature)) as temperature ,round(avg(pm1)) as pm1, round(avg(pm25)) as 'pm2.5' ,round(avg(pm10)) as pm10  FROM measurements WHERE device_id=${id} GROUP BY measurement_date;`;
   const result = await dbQuery(query);
   res.send(result);
 };
