2fb7cb2623c97857c90d1b9f464b9609d315eb34
obx88 <33556172+obx88@users.noreply.github.com>
2020-03-19 16:22:12 +0100

    parsing added

--- src/pages/api/time.js
+++ src/pages/api/time.js
@@ -2,11 +2,11 @@ const moment = require('moment');
 
 export default async (req, res) => {
   res.json({
-    seconds: moment().format('s'),
-    minutes: moment().format('m'),
-    hours: moment().format('h'),
-    day: moment().format('D'),
-    month: moment().format('M'),
-    year: moment().format('YYYY'),
+    seconds: parseInt(moment().format('s'), 10),
+    minutes: parseInt(moment().format('m'), 10),
+    hours: parseInt(moment().format('h'),10),
+    day: parseIntmoment(().format('D'),10),
+    month: parseIntmoment(().format('M'),10),
+    year: parseIntmoment(().format('YYYY'),10),
   });
 };
