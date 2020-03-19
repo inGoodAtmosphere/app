dedd462ae935e38b556d9e58a846279716e94ac0
obx88 <33556172+obx88@users.noreply.github.com>
2020-03-19 15:26:13 +0100

    formatted time properly on api/time endpoint

--- src/pages/api/time.js
+++ src/pages/api/time.js
@@ -2,11 +2,11 @@ const moment = require('moment');
 
 export default async (req, res) => {
   res.json({
-    seconds: moment().format('ss'),
-    minutes: moment().format('mm'),
-    hours: moment().format('hh'),
-    day: moment().format('DD'),
-    month: moment().format('MM'),
+    seconds: moment().format('s'),
+    minutes: moment().format('m'),
+    hours: moment().format('h'),
+    day: moment().format('D'),
+    month: moment().format('M'),
     year: moment().format('YYYY'),
   });
 };
