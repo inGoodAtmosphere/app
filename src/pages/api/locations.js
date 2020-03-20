64e9cb424ee2530efa19529134b1f49efe31238a
obx88 <33556172+obx88@users.noreply.github.com>
2020-03-20 20:16:14 +0100

    query changed

--- src/pages/api/locations.js
+++ src/pages/api/locations.js
@@ -2,7 +2,7 @@ const dbQuery = require('../../../api_modules/selectQuery');
 
 export default async (req, res) => {
   const query =
-    'SELECT `id`,`Location_Latitude` as lat,`Location_Longitude` as lng from devices limit 50';
+    'SELECT `id`,`Location_Latitude` as lat,`Location_Longitude` as lng from devices';
   const result = await dbQuery(query);
   res.send(result);
 };
