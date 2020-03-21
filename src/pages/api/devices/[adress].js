2f18774c9ce78130b1e65d43387f4c0fc6b9e3c3
obx88 <33556172+obx88@users.noreply.github.com>
2020-03-21 21:51:47 +0100

    added function of adding new devices to db

--- src/pages/api/devices/[adress].js
+++ src/pages/api/devices/[adress].js
@@ -3,26 +3,31 @@ const dbQuery = require('../../../../api_modules/selectQuery');
 export default async (req, res) => {
   let query = '';
   let result = '';
-  const checkIfAdressRegistered = {};
+  let checkIfAdressRegistered = {};
   const {
     query: { adress },
   } = req;
   switch (req.method) {
     case 'POST':
-      query = `select id from devices where Macadress=${db.escape(adress.replace("U","F").replace("V","E").replace("W","D").replace("X","C").replace("Y","T").replace("Z","A"))}`;
+      query = `select id from devices where Macadress=${dbQuery.escape(
+        adress
+          .replace('U', 'F')
+          .replace('V', 'E')
+          .replace('W', 'D')
+          .replace('X', 'C')
+          .replace('Y', 'T')
+          .replace('Z', 'A'),
+      )}`;
       checkIfAdressRegistered = await dbQuery(query);
-      if(checkIfAdressRegistered.length ===0)
-      {
-          query=`insert into devices(Macadress, Location_Latitude, Location_Longitude) values(${adress}, ${req.body.locationLatitude}, ${req.body.locationLongitude})`;
-          result = await dbQuery(query);
-          res.send(result);
-      } else if(checkIfAdressRegistered.length===1)
-      {
-          result=checkIfAdressRegistered;
-          res.send(result);
-      }
-      else{
-          res.json({ message: "Data error"});
+      if (checkIfAdressRegistered.length === 0) {
+        query = `insert into devices(Macadress, Location_Latitude, Location_Longitude) values(${adress}, ${req.body.locationLatitude}, ${req.body.locationLongitude})`;
+        result = await dbQuery(query);
+        res.send(result);
+      } else if (checkIfAdressRegistered.length === 1) {
+        result = checkIfAdressRegistered;
+        res.send(result);
+      } else {
+        res.json({ message: 'Data error' });
       }
       break;
     default:
