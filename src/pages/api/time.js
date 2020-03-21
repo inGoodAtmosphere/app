67c77727e5ba3a0381a5016b50d7487c710c3c97
obx88 <33556172+obx88@users.noreply.github.com>
2020-03-21 17:04:31 +0100

    hour format changed

--- src/pages/api/time.js
+++ src/pages/api/time.js
@@ -3,11 +3,11 @@ const moment = require('moment');
 export default async (req, res) => {
   if (req.method === 'GET') {
     res.json({
-      seconds: parseInt(moment().format('s'), 10),
-      minutes: parseInt(moment().format('m'), 10),
-      hours: parseInt(moment().format('H'), 10),
-      day: parseInt(moment().format('D'), 10),
-      month: parseInt(moment().format('M'), 10),
+      seconds: parseInt(moment().format('ss'), 10),
+      minutes: parseInt(moment().format('mm'), 10),
+      hours: parseInt(moment().format('HH'), 10),
+      day: parseInt(moment().format('DD'), 10),
+      month: parseInt(moment().format('MM'), 10),
       year: parseInt(moment().format('YYYY'), 10),
     });
   } else {
