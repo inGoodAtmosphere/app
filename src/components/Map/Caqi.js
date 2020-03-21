da3aa0a076057e727ece6199cfd2e797589fec84
Jakub Ciepły <jakubc01@gmail.com>
2020-03-21 01:50:46 +0100

    Fix CAQI descriptions

--- src/components/Map/Caqi.js
+++ src/components/Map/Caqi.js
@@ -10,12 +10,13 @@ const Caqi = () => {
   } = useContext(mapContext);
   const [currentMeasurement] = avg;
   const caqi = countCaqi(currentMeasurement['pm2.5'], currentMeasurement.pm10);
-  const [bad, medium, good, veryGood] = descriptions;
+  const [veryBad, bad, medium, good, veryGood] = descriptions;
   const setStatus = (caqiValue) => {
-    if (caqiValue < 20) return { backgroundColor: '#44a368', status: veryGood };
-    if (caqiValue < 40) return { backgroundColor: '#a9c46e', status: good };
-    if (caqiValue < 60) return { backgroundColor: '#c19330', status: medium };
-    return { backgroundColor: '#e1625a', status: bad };
+    if (caqiValue < 25) return { backgroundColor: '#44a368', status: veryGood };
+    if (caqiValue < 50) return { backgroundColor: '#a9c46e', status: good };
+    if (caqiValue < 75) return { backgroundColor: '#c19330', status: medium };
+    if (caqiValue < 100) return { backgroundColor: '#e1625a', status: bad };
+    return { backgroundColor: '#7c1d7a', status: veryBad };
   };
   const { status, backgroundColor } = setStatus(caqi);
   return (
