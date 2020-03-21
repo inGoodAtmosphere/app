804ac3f97a85991eeb5734dbe0d0449320aab541
obx88 <33556172+obx88@users.noreply.github.com>
2020-03-21 16:54:03 +0100

    hour format changed

--- src/pages/api/time.js
+++ src/pages/api/time.js
@@ -5,7 +5,7 @@ export default async (req, res) => {
     res.json({
       seconds: parseInt(moment().format('s'), 10),
       minutes: parseInt(moment().format('m'), 10),
-      hours: parseInt(moment().format('h'), 10),
+      hours: parseInt(moment().format('H'), 10),
       day: parseInt(moment().format('D'), 10),
       month: parseInt(moment().format('M'), 10),
       year: parseInt(moment().format('YYYY'), 10),
