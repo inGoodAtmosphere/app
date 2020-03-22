66d855db108a144d4a35da73f9e2d7251aa53843
Jakub Ciepły <jakubc01@gmail.com>
2020-03-22 17:04:42 +0100

    Add login form

---
+++ src/pages/admin/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from '../../components/Form';
+
+const Admin = () => {
+  return (
+    <main className="content admin__content">
+      <Form />
+    </main>
+  );
+};
+
+export default Admin;
