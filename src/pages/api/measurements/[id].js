d362e9cbc4803805c59afb38e6c2e69e82bbd4f2
obx88 <33556172+obx88@users.noreply.github.com>
2020-03-22 21:25:31 +0100

    added

--- src/pages/api/measurements/[id].js
+++ src/pages/api/measurements/[id].js
@@ -1,8 +1,8 @@
 const dbQuery = require('../../../../api_modules/selectQuery');
 
 export default async (req, res) => {
-  let checkIfDeviceRegisteredQuery={};
-  let checkIfDeviceRegistered=[];
+  let checkIfDeviceRegisteredQuery = {};
+  let checkIfDeviceRegistered = [];
   let query = '';
   let result = '';
   const {
@@ -19,10 +19,11 @@ export default async (req, res) => {
       result = await dbQuery(query);
       break;
     case 'POST':
-      checkIfDeviceRegisteredQuery =`Select id from devices where id=${dbQuery.escape(id)}`
-      checkIfDeviceRegistered=dbQuery(checkIfDeviceRegisteredQuery);
-      if(checkIfDeviceRegistered===1)
-      {
+      checkIfDeviceRegisteredQuery = `Select id from devices where id=${dbQuery.escape(
+        id,
+      )}`;
+      checkIfDeviceRegistered = dbQuery(checkIfDeviceRegisteredQuery);
+      if (checkIfDeviceRegistered === 1) {
         query = `INSERT INTO measurements(device_id,measurement_date, measurement_time, receive_date,receive_time, humidity, temperature, pm1,pm25,pm10) values(${dbQuery.escape(
           id,
         )},${dbQuery.escape(req.body.measurementDate)},${dbQuery.escape(
@@ -33,13 +34,10 @@ export default async (req, res) => {
           req.body.pm1,
         )},${dbQuery.escape(req.body.pm25)},${dbQuery.escape(req.body.pm10)})`;
         result = await dbQuery(query);
-      }
-      else if(checkIfDeviceRegistered===0)
-      {
-        result={error: "Device not registered"}
-      }
-      else{
-        result={error: "Data error"};
+      } else if (checkIfDeviceRegistered === 0) {
+        result = { error: 'Device not registered' };
+      } else {
+        result = { error: 'Data error' };
       }
       break;
     default:
