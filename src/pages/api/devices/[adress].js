879896f8e262ef3d448002bf5896d308b6a62b21
obx88 <33556172+obx88@users.noreply.github.com>
2020-03-23 20:46:09 +0100

    encryption fixed

--- src/pages/api/devices/[adress].js
+++ src/pages/api/devices/[adress].js
@@ -2,6 +2,7 @@ const dbQuery = require('../../../../api_modules/selectQuery');
 
 export default async (req, res) => {
   let query = '';
+  let queryTest = '';
   let result = '';
   let checkIfAdressRegistered = {};
   const {
@@ -9,34 +10,39 @@ export default async (req, res) => {
   } = req;
   switch (req.method) {
     case 'POST':
-      query = `select id from devices where Macadress=${dbQuery.escape(
-        adress
-          .replace('U', 'F')
-          .replace('V', 'E')
-          .replace('W', 'D')
-          .replace('X', 'C')
-          .replace('Y', 'T')
-          .replace('Z', 'A'),
-      )}`;
-      checkIfAdressRegistered = await dbQuery(query);
-      if (checkIfAdressRegistered.length === 0) {
-        query = `insert into devices(Macadress, Location_Latitude, Location_Longitude) values(${dbQuery.escape(
-          adress,
-        )}, ${dbQuery.escape(req.body.locationLatitude)}, ${dbQuery.escape(
-          req.body.locationLongitude,
-        )})`;
-        result = await dbQuery(query);
-        res.send(result);
-      } else if (checkIfAdressRegistered.length === 1) {
-        result = checkIfAdressRegistered;
-        res.send(result);
-      } else {
-        res.json({ error: 'Data error' });
+      if(adress.length!=17)
+      {
+        res.json({error:"wrong input"});
+      }
+      else {
+        query = `select id from devices where Macadress=${dbQuery.escape(
+          adress
+            .replace('U', 'F')
+            .replace('V', 'E')
+            .replace('W', 'D')
+            .replace('X', 'C')
+            .replace('Y', 'B')
+            .replace('Z', 'A'),
+        )}`;
+        checkIfAdressRegistered = await dbQuery(query);
+        if (checkIfAdressRegistered.length === 0) {
+          queryTest = `insert into devices(Macadress, Location_Latitude, Location_Longitude) values(${dbQuery.escape(
+            adress,
+          )}, ${dbQuery.escape(req.body.locationLatitude)}, ${dbQuery.escape(
+            req.body.locationLongitude,
+          )})`;
+          result = await dbQuery(queryTest);
+          res.send(result.insertId);
+        } else if (checkIfAdressRegistered.length === 1) {
+          result = checkIfAdressRegistered;
+          res.send(result);
+        } else {
+          res.json({ error: 'Data error' });
+        }
       }
       break;
     default:
       res.status(403);
       res.end();
   }
-  res.send(result);
 };
