13029dae07bb07c7477689fe53854dd59d1835de
obx88 <33556172+obx88@users.noreply.github.com>
2020-03-30 00:08:35 +0200

    validation added

--- src/pages/api/sendMail.js
+++ src/pages/api/sendMail.js
@@ -8,33 +8,34 @@ export default async (req, res) => {
 
     // validation shit here
     const errors = [];
-    if(!req.body.email){
-      errors.push("You must fill an email field");
+    if (!req.body.email) {
+      errors.push('You must fill an email field');
     }
-    if(!req.body.name){
-      errors.push("You must fill your name field");
+    if (!req.body.name) {
+      errors.push('You must fill your name field');
     }
-    if(!req.body.content){
-      errors.push("Message cannot be empty");
-    } else if(req.body.content.length<10){
-      errors.push("Message content too short")
+    if (!req.body.content) {
+      errors.push('Message cannot be empty');
+    } else if (req.body.content.length < 10) {
+      errors.push('Message content too short');
     }
-    const subject = req.body.subject || "brak tematu";
-
+    const subject = req.body.subject || 'brak tematu';
 
     const recipient = 'ingoodatmosphere@gmail.com';
     // let recipient;
     const plainContent = `Od:${req.body.name} adres email: ${req.body.email} ${req.body.content}`;
-    errors.concat(await sendMail(
-      recipient,
-      `Kontakt: ${subject}`,
-      plainContent,
-      `${req.body.name} do`,
-    ).catch((error) => {
-      console.log(error);
-      res.status(500);
-      res.end();
-    }));
+    errors.concat(
+      await sendMail(
+        recipient,
+        `Kontakt: ${subject}`,
+        plainContent,
+        `${req.body.name} do`,
+      ).catch((error) => {
+        console.log(error);
+        res.status(500);
+        res.end();
+      }),
+    );
     console.log(errors);
     if (!errors) {
       res.json({
