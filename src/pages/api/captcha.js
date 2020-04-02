ce852a2446cc95362d1c55ec21bd06dbd27134be
obx88 <33556172+obx88@users.noreply.github.com>
2020-04-02 22:05:20 +0200

    hotfix

--- src/pages/api/captcha.js
+++ src/pages/api/captcha.js
@@ -1,15 +1,18 @@
 require('dotenv').config();
 const fetch = require('isomorphic-fetch');
 
-export default (req, res) => {
+export default async (req, res) => {
   const secretKey = process.env.CAPTCHA_SECRET_KEY;
   const token = req.body;
   const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`;
 
-  fetch(url, {
-    method: 'post',
-  })
-    .then((response) => response.json())
-    .then((googleResponse) => res.json({ googleResponse }))
-    .catch((error) => res.json({ error }));
+  try {
+    const response = await fetch(url, {
+      method: 'post',
+    });
+    const googleResponse = await response.json();
+    res.json(googleResponse);
+  } catch (error) {
+    res.json({ error });
+  }
 };
