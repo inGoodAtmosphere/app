ec4408a81d2fc0a51f3af77892f20747013ac5c4
obx88 <33556172+obx88@users.noreply.github.com>
2020-04-02 21:19:20 +0200

    added captcha endpoint

---
+++ src/pages/api/captcha.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const fetch = require('isomorphic-fetch');
+
+export default (req, res) => {
+    const secretKey = process.env.SECRET_KEY;
+    const {token} = req.body;
+    const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`;
+
+    fetch(url, {
+        method: 'post'
+    })
+        .then(response => response.json())
+        .then(googleResponse => res.json({ googleResponse }))
+        .catch(error => res.json({ error }));
+};
