c0862a9ee6109c7daba6153787d92d345c5234e0
Jakub Ciepły <jakubc01@gmail.com>
2020-04-03 17:14:03 +0200

    Change hero image

--- src/components/LandingPage/HeroImage.js
+++ src/components/LandingPage/HeroImage.js
@@ -6,8 +6,8 @@ const HeroImage = () => {
     <div className="landing-page__hero">
       <img
         className="landing-page__hero__image"
-        src="/img/hero-images/hero-image-1280.jpg"
-        srcSet="/img/hero-images/hero-image-640.jpg 640w, /img/hero-images/hero-image-1280.jpg 1280w,/img/hero-images/hero-image-1920.jpg 1920w,/img/hero-images/hero-image-5266.jpg 5266w"
+        src="/img/hero-images/1280.jpg"
+        srcSet="/img/hero-images/640.jpg 640w, /img/hero-images/1280.jpg 1280w,/img/hero-images/1920.jpg 1920w,/img/hero-images/4896.jpg 4896w"
         alt="Hero"
       />
       <h1 className="landing-page__hero__text">
