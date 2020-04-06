965b90be9601173bcbdb1c0397d4e37a7ecd412d
Jakub Ciepły <jakubc01@gmail.com>
2020-04-06 15:19:47 +0200

    Add service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,75 @@
-const CACHE_NAME = 'simple-cache-v1';
-const urlsToCache = ['/'];
+const staticCacheName = 'site-static-v1';
+const dynamicCacheName = 'site-dynamic-v1';
+const assets = [
+  '/',
+  '/offline',
+  'https://fonts.googleapis.com/css?family=Lato:400,700&display=swap&subset=latin-ext',
+  '/_next/static/css/styles.6f9dd3a5.chunk.css',
+  '/_next/static/2PizZtLiDgO2o6I7Ed102/pages/index.js',
+  '/_next/static/2PizZtLiDgO2o6I7Ed102/pages/_app.js',
+  '/_next/static/2PizZtLiDgO2o6I7Ed102/pages/_error.js',
+  '/img/hero-images/1280.jpg',
+  '/img/gliwice.png',
+  '/img/tvp.png',
+  '/img/msk.png',
+  '/img/botland.png',
+];
+
+const limitCacheSize = (name, size) => {
+  caches.open(name).then((cache) => {
+    cache.keys().then((keys) => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
 
 self.addEventListener('install', (event) => {
-  const preLoaded = caches
-    .open(CACHE_NAME)
-    .then((cache) => cache.addAll(urlsToCache));
-  event.waitUntil(preLoaded);
+  event.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    }),
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter(
+              (key) => key !== staticCacheName && key !== dynamicCacheName,
+            )
+            .map((key) => caches.delete(key)),
+        ),
+      ),
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-  const response = caches
-    .match(event.request)
-    .then((match) => match || fetch(event.request));
-  event.respondWith(response);
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(
+        (cacheRes) =>
+          cacheRes ||
+          fetch(event.request).then(async (fetchRes) => {
+            const cache = await caches.open(dynamicCacheName);
+            if (!/^https?:$/i.test(new URL(event.request.url).protocol)) return;
+            cache.put(event.request.url, fetchRes.clone());
+            // check cached items size
+            limitCacheSize(dynamicCacheName, 20);
+            return fetchRes;
+          }),
+      )
+      .catch(() => {
+        if (event.request.destination === 'unknown') {
+          return caches.match('/offline');
+        }
+      }),
+  );
 });
---
+++ src/pages/offline/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Error from '../../components/Error';
+
+const Offline = () => {
+  return <Error message="Offline" />;
+};
+
+export default Offline;
