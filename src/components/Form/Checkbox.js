2c2a93cdbcd02934d5d7006fbbfc2667796d12ca
Jakub Ciepły <jakubc01@gmail.com>
2020-04-13 19:29:26 +0200

    Fix checkbox

--- src/components/Form/Checkbox.js
+++ src/components/Form/Checkbox.js
@@ -1,37 +1,37 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import './checkbox.module.scss';
 
-const Checkbox = ({ name }) => {
-  const [isChecked, setIsChecked] = useState(false);
-  const handleClick = () => {
+const Checkbox = ({ ariaLabel, name, isChecked, setIsChecked }) => {
+  const handleChange = () => {
     setIsChecked(!isChecked);
   };
   return (
-    <>
-      <label htmlFor="checkbox" className="form__checkbox">
-        <div className="form__checkbox__container">
-          <input
-            type="checkbox"
-            name={name}
-            className="form__checkbox__check"
-            onClick={handleClick}
-          />
-          <div className="form__checkbox__check--styled">
-            <FontAwesomeIcon icon={faCheck} />
-          </div>
+    <label htmlFor={name} className="form__checkbox">
+      <div className="form__checkbox__container">
+        <input
+          type="checkbox"
+          aria-label={ariaLabel}
+          id={name}
+          name={name}
+          className="form__checkbox__check"
+          checked={isChecked}
+          onChange={handleChange}
+        />
+        <div className="form__checkbox__check--styled">
+          <FontAwesomeIcon icon={faCheck} />
         </div>
-        Zapamiętaj mnie
-      </label>
-    </>
+      </div>
+      Zapamiętaj mnie
+    </label>
   );
 };
-
 Checkbox.propTypes = {
+  ariaLabel: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  isChecked: PropTypes.bool.isRequired,
+  setIsChecked: PropTypes.func.isRequired,
 };
-
 export default Checkbox;
