5082c61404596e2f0fc323c84f245bbcf648fda3
Olivier <olivierkus88@gmail.com>
2020-04-15 20:13:48 +0200

    dbquery fixed

--- src/pages/api/api.js
+++ src/pages/api/api.js
@@ -1,15 +1,1 @@
-// const express = require('express');
-
-// const {router} = express;
-
-const requests = [];
-let emptyRequests = 0;
-
-export default (req, res) => {
-  if (req.body === 'clear') {
-    requests.splice(0, requests.length);
-    emptyRequests = 0;
-  } else if (req.body === '') emptyRequests += 1;
-  else requests.push(req.body);
-  res.send(`${emptyRequests}\r\n${requests}`);
-};
+import dbQuery from '../../../api_modules/dbQuery';
