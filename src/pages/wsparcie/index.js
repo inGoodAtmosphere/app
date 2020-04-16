d99df67d999c690785127d162a5666adefa74e41
Jakub Ciepły <jakubc01@gmail.com>
2020-04-16 20:37:08 +0200

    Refactor and add animation to support card

--- src/pages/wsparcie/index.js
+++ src/pages/wsparcie/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './index.module.scss';
 
 const Support = () => {
   return (
-    <main className="content">
+    <main className={styles.content}>
       <h1>Wesprzyj nas</h1>
       <h2>Lorem ipsum dolor sit amet consectetur adipisicing.</h2>
       <div>donorbox</div>
