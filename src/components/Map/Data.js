36022fb9bf2f41dbf968cf753af914d39ed7acb2
Jakub Ciepły <jakubc01@gmail.com>
2020-04-23 13:39:31 +0200

    Handle error with active sensor

--- src/components/Map/Data.js
+++ src/components/Map/Data.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Measurements from './Measurements';
 import Weather from './Weather';
 import AboutDevice from './AboutDevice';
+import Error from '../Error';
 import withContext from '../../utils/withContext';
+import MapContext from '../../utils/map-context';
 import styles from './Data.module.scss';
 
 const Data = () => {
+  const {
+    activeSensor: { status, data },
+  } = useContext(MapContext);
   return (
-    <div className={styles.data}>
-      <Measurements />
-      <Weather />
-      <AboutDevice />
+    <div className={` ${status === 'ok' ? styles.data : styles.error}`}>
+      {status === 'ok' ? (
+        <>
+          <Measurements />
+          <Weather />
+          <AboutDevice />
+        </>
+      ) : (
+        <Error text="Ten czujnik jest niedostępny" status={500} label={data} />
+      )}
     </div>
   );
 };
