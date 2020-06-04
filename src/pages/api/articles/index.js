c6cdb6fbc454c506e306d6ce3c54da75c3ee0cfd
Jakub Ciepły <jakubc01@gmail.com>
2020-06-04 17:57:01 +0200

    modify api

--- src/pages/api/articles/index.js
+++ src/pages/api/articles/index.js
@@ -3,7 +3,7 @@ const dbQuery = require('../../../../api_modules/dbQuery');
 
 export default async (req, res) => {
   const query =
-    'SELECT id, header, description, link, images, tags from articles order by id desc';
+    'SELECT id, header, subheader description, link, images, tags from articles order by id desc';
   const result = await dbQuery(query);
   res.send(result);
 };
